73f837ece47391b7f7d8e848af0b19eac21a5b67
sunday00 <kind65505256@gmail.com>
2024-05-27 03:58:49 +0900

    :lipstick: Prettier

--- nodejs/grpc-series/ts-grpc-try2/src/domain/hero.ts
+++ nodejs/grpc-series/ts-grpc-try2/src/domain/hero.ts
@@ -1,23 +1,37 @@
-import type * as grpc from '@grpc/grpc-js';
-import type { MessageTypeDefinition } from '@grpc/proto-loader';
+import type * as grpc from '@grpc/grpc-js'
+import type { MessageTypeDefinition } from '@grpc/proto-loader'
 
-import type { HeroesServiceClient as _hero_HeroesServiceClient, HeroesServiceDefinition as _hero_HeroesServiceDefinition } from './hero/HeroesService';
-import type { HeroesServiceClient as _hero2_HeroesServiceClient, HeroesServiceDefinition as _hero2_HeroesServiceDefinition } from './hero2/HeroesService';
+import type {
+  HeroesServiceClient as _hero_HeroesServiceClient,
+  HeroesServiceDefinition as _hero_HeroesServiceDefinition,
+} from './hero/HeroesService'
+import type {
+  HeroesServiceClient as _hero2_HeroesServiceClient,
+  HeroesServiceDefinition as _hero2_HeroesServiceDefinition,
+} from './hero2/HeroesService'
 
-type SubtypeConstructor<Constructor extends new (...args: any) => any, Subtype> = {
-  new(...args: ConstructorParameters<Constructor>): Subtype;
-};
+type SubtypeConstructor<
+  Constructor extends new (...args: any) => any,
+  Subtype,
+> = {
+  new (...args: ConstructorParameters<Constructor>): Subtype
+}
 
 export interface ProtoGrpcType {
   hero: {
     Hero: MessageTypeDefinition
     HeroById: MessageTypeDefinition
-    HeroesService: SubtypeConstructor<typeof grpc.Client, _hero_HeroesServiceClient> & { service: _hero_HeroesServiceDefinition }
+    HeroesService: SubtypeConstructor<
+      typeof grpc.Client,
+      _hero_HeroesServiceClient
+    > & { service: _hero_HeroesServiceDefinition }
   }
   hero2: {
     Hero: MessageTypeDefinition
     HeroById: MessageTypeDefinition
-    HeroesService: SubtypeConstructor<typeof grpc.Client, _hero2_HeroesServiceClient> & { service: _hero2_HeroesServiceDefinition }
+    HeroesService: SubtypeConstructor<
+      typeof grpc.Client,
+      _hero2_HeroesServiceClient
+    > & { service: _hero2_HeroesServiceDefinition }
   }
 }
-
