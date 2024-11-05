f52ec33a5ee76feb02c82e029074c256815b6d32
sunday00 <kind65505256@gmail.com>
2024-11-05 23:59:22 +0900

    basic stream

--- nodejs/rxjs-complete-udm/ch1-concept/src/app/about/about.component.ts
+++ nodejs/rxjs-complete-udm/ch1-concept/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core'
+import { fromEvent, interval, timer } from 'rxjs'
 
 @Component({
   selector: 'about',
@@ -9,19 +10,39 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    document.addEventListener('click', (event) => {
-      console.log(event)
+    // document.addEventListener('click', (event) => {
+    //   console.log(event)
+    //
+    //   setTimeout(() => {
+    //     console.log('fin')
+    //
+    //     let counter = 0
+    //     setInterval(() => {
+    //       console.log(counter)
+    //
+    //       counter++
+    //     }, 1000)
+    //   }, 3000)
+    // })
 
-      setTimeout(() => {
-        console.log('fin')
+    // const interval$ = interval(1000)
+    const interval$ = timer(3000, 1000)
 
-        let counter = 0
-        setInterval(() => {
-          console.log(counter)
+    // interval$.subscribe((val) => console.log(`stream2 ${val}`))
 
-          counter++
-        }, 1000)
-      }, 3000)
-    })
+    const click$ = fromEvent(document, 'click')
+    click$.subscribe(
+      (evt) => {
+        console.log(evt)
+
+        const sub = interval$.subscribe((val) => console.log(`stream1 ${val}`))
+
+        setTimeout(() => {
+          sub.unsubscribe()
+        }, 10000)
+      }, // try
+      (err) => console.log(err), // catch
+      () => console.log('complete'), // finally
+    )
   }
 }
