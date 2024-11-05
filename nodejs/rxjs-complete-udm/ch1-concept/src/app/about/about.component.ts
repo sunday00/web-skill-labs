70a31fe55b3aaf9e918941ce70d17ba59bedf090
sunday00 <kind65505256@gmail.com>
2024-11-06 00:15:27 +0900

    first stram with fetch

--- nodejs/rxjs-complete-udm/ch1-concept/src/app/about/about.component.ts
+++ nodejs/rxjs-complete-udm/ch1-concept/src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core'
-import { fromEvent, interval, timer } from 'rxjs'
+import { fromEvent, interval, noop, Observable, timer } from 'rxjs'
 
 @Component({
   selector: 'about',
@@ -24,25 +24,43 @@ export class AboutComponent implements OnInit {
     //     }, 1000)
     //   }, 3000)
     // })
+    // // const interval$ = interval(1000)
+    // const interval$ = timer(3000, 1000)
+    //
+    // // interval$.subscribe((val) => console.log(`stream2 ${val}`))
+    //
+    // const click$ = fromEvent(document, 'click')
+    // click$.subscribe(
+    //   (evt) => {
+    //     console.log(evt)
+    //
+    //     const sub = interval$.subscribe((val) => console.log(`stream1 ${val}`))
+    //
+    //     setTimeout(() => {
+    //       sub.unsubscribe()
+    //     }, 10000)
+    //   }, // try
+    //   (err) => console.log(err), // catch
+    //   () => console.log('complete'), // finally
+    // )
 
-    // const interval$ = interval(1000)
-    const interval$ = timer(3000, 1000)
+    const http$ = new Observable((observer) => {
+      fetch('/api/courses')
+        .then((res) => res.json())
+        .then((body) => {
+          observer.next(body)
+          observer.complete()
+        })
+        .catch((error) => {
+          observer.error(error)
+        })
+    })
 
-    // interval$.subscribe((val) => console.log(`stream2 ${val}`))
-
-    const click$ = fromEvent(document, 'click')
-    click$.subscribe(
-      (evt) => {
-        console.log(evt)
-
-        const sub = interval$.subscribe((val) => console.log(`stream1 ${val}`))
-
-        setTimeout(() => {
-          sub.unsubscribe()
-        }, 10000)
-      }, // try
-      (err) => console.log(err), // catch
-      () => console.log('complete'), // finally
+    http$.subscribe(
+      (c) => console.log(c),
+      // () => {},
+      noop,
+      () => console.log('done'),
     )
   }
 }
