dc745080c47198c86098dd42431508b18b407d60
sunday00 <kind65505256@gmail.com>
2024-11-07 00:21:21 +0900

    concat

--- nodejs/rxjs-complete-udm/ch1-concept/src/app/about/about.component.ts
+++ nodejs/rxjs-complete-udm/ch1-concept/src/app/about/about.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core'
-import { fromEvent, interval, noop, Observable, timer } from 'rxjs'
-import { createHttpObservable } from '../common/util'
-import { map } from 'rxjs/operators'
+import { Component, OnInit } from '@angular/core'
+import { asyncScheduler, of, scheduled } from 'rxjs'
+import { concatAll } from 'rxjs/operators'
 
 @Component({
   selector: 'about',
@@ -11,5 +10,14 @@ import { map } from 'rxjs/operators'
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const source1$ = of(1, 2, 3)
+    // const source1$ = interval(1000)
+    const source2$ = of(4, 5, 6)
+    const source3$ = of(7, 8, 9)
+
+    // const r$ = concat(source1$, source2$, source3$)
+    const r$ = scheduled([source1$, source2$, source3$], asyncScheduler).pipe(concatAll())
+    r$.subscribe(console.log)
+  }
 }
