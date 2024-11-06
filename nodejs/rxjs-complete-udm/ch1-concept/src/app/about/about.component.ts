be73d740a3d4c79ae0098304b88dc68dadbbd8b4
sunday00 <kind65505256@gmail.com>
2024-11-06 22:58:02 +0900

    map

--- nodejs/rxjs-complete-udm/ch1-concept/src/app/about/about.component.ts
+++ nodejs/rxjs-complete-udm/ch1-concept/src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core'
 import { fromEvent, interval, noop, Observable, timer } from 'rxjs'
+import { createHttpObservable } from '../common/util'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'about',
@@ -10,53 +12,11 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    // document.addEventListener('click', (event) => {
-    //   console.log(event)
-    //
-    //   setTimeout(() => {
-    //     console.log('fin')
-    //
-    //     let counter = 0
-    //     setInterval(() => {
-    //       console.log(counter)
-    //
-    //       counter++
-    //     }, 1000)
-    //   }, 3000)
-    // })
-    // // const interval$ = interval(1000)
-    // const interval$ = timer(3000, 1000)
-    //
-    // // interval$.subscribe((val) => console.log(`stream2 ${val}`))
-    //
-    // const click$ = fromEvent(document, 'click')
-    // click$.subscribe(
-    //   (evt) => {
-    //     console.log(evt)
-    //
-    //     const sub = interval$.subscribe((val) => console.log(`stream1 ${val}`))
-    //
-    //     setTimeout(() => {
-    //       sub.unsubscribe()
-    //     }, 10000)
-    //   }, // try
-    //   (err) => console.log(err), // catch
-    //   () => console.log('complete'), // finally
-    // )
+    const http$ = createHttpObservable('/api/courses')
 
-    const http$ = new Observable((observer) => {
-      fetch('/api/courses')
-        .then((res) => res.json())
-        .then((body) => {
-          observer.next(body)
-          observer.complete()
-        })
-        .catch((error) => {
-          observer.error(error)
-        })
-    })
+    const courses$ = http$.pipe(map((r) => Object.values(r['payload'])))
 
-    http$.subscribe(
+    courses$.subscribe(
       (c) => console.log(c),
       // () => {},
       noop,
--- nodejs/rxjs-complete-udm/ch1-concept/src/app/common/util.ts
+++ nodejs/rxjs-complete-udm/ch1-concept/src/app/common/util.ts
@@ -1,2 +1,15 @@
+import { Observable } from 'rxjs'
 
-
+export function createHttpObservable(url: string) {
+  return new Observable((observer) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((body) => {
+        observer.next(body)
+        observer.complete()
+      })
+      .catch((error) => {
+        observer.error(error)
+      })
+  })
+}
