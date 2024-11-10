d48add617bc5b8ccdb32d20cfda298a68c30aec9
sunday00 <kind65505256@gmail.com>
2024-11-10 23:33:25 +0900

    lazy subject, behavior Subject

--- nodejs/rxjs-complete-udm/ch2-store/src/app/about/about.component.ts
+++ nodejs/rxjs-complete-udm/ch2-store/src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Subject } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
   selector: "about",
@@ -8,16 +8,26 @@ import { Subject } from "rxjs";
 })
 export class AboutComponent implements OnInit {
   ngOnInit() {
-    const subject = new Subject();
+    // const subject = new Subject();
+    const subject = new BehaviorSubject(0);
 
     // subject.complete()
     const series$ = subject.asObservable();
 
-    series$.subscribe(console.log);
+    series$.subscribe((val) => console.log("early : ", val));
 
     subject.next(1);
     subject.next(2);
     subject.next(3);
+
     subject.complete();
+
+    setTimeout(() => {
+      series$.subscribe((val) => console.log("lazily : ", val));
+
+      subject.next(4);
+      subject.next(5);
+      subject.next(6);
+    }, 3000);
   }
 }
