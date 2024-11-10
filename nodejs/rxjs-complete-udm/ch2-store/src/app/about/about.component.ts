7e522ae52923da679f53d1f160bb560bf7555c66
sunday00 <kind65505256@gmail.com>
2024-11-10 22:11:08 +0900

    subject

---
+++ nodejs/rxjs-complete-udm/ch2-store/src/app/about/about.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
+
+@Component({
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
+})
+export class AboutComponent implements OnInit {
+  ngOnInit() {
+    const subject = new Subject();
+
+    // subject.complete()
+    const series$ = subject.asObservable();
+
+    series$.subscribe(console.log);
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    subject.complete();
+  }
+}
