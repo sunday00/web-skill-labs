38dc6e1cde5eb34473ada2991dee51d5565e9747
sunday00 <kind65505256@gmail.com>
2024-11-25 01:15:33 +0900

    complete

--- nodejs/blockchain-nomad/03-project/src/index.ts
+++ nodejs/blockchain-nomad/03-project/src/index.ts
@@ -1,15 +1,59 @@
-import { init } from 'my-package'
-import { hello } from './normal'
+import crypto from 'node:crypto'
 
-class Block {
-  constructor(private data: string) {}
+interface BlockShape {
+  hash: string
+  prevHash: string
+  height: number
+  data: string
+}
 
-  static Hello() {
-    return 'hi!!'
+class Block implements BlockShape {
+  public hash: string
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string,
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data)
+  }
+
+  public static calculateHash(
+    prevHash: string,
+    height: number,
+    data: string,
+  ): string {
+    return crypto
+      .createHash('sha-256')
+      .update(`${prevHash}${height}${data}`)
+      .digest('hex')
   }
 }
 
-console.log(Block.Hello())
+class BlockChain {
+  private blocks: Block[] = []
+  constructor() {
+    this.blocks = []
+  }
 
-init()
-hello('123')
+  public addBlock(data: string): void {
+    const block = new Block(this.getPrevHash(), this.blocks.length + 1, data)
+    this.blocks.push(block)
+  }
+
+  public getBlocks() {
+    return [...this.blocks]
+  }
+
+  private getPrevHash(): string {
+    if (this.blocks.length == 0) return ''
+    return this.blocks[this.blocks.length - 1].hash
+  }
+}
+
+const blockChain = new BlockChain()
+blockChain.addBlock('first one')
+blockChain.addBlock('second one')
+blockChain.addBlock('3rd one')
+blockChain.addBlock('4 one')
+
+console.log(blockChain.getBlocks())
