d48e69ab69e8fdfc1922d30f03fe4e5d66f0b61f
sunday00 <kind65505256@gmail.com>
2024-12-25 21:56:03 +0900

    deref

--- rust/tuto-official/ch15-pointer/src/lib.rs
+++ rust/tuto-official/ch15-pointer/src/lib.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 pub mod a_box;
+pub mod b_deref;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
@@ -10,6 +11,11 @@ pub fn run() {
         "box" => {
             if args.get(2).unwrap().eq("1") {
                 a_box::ch1::exec();
+            }
+        }
+        "deref" => {
+            if args.get(2).unwrap().eq("1") {
+                b_deref::ch1::exec();
             }
         }
         _ => {}
