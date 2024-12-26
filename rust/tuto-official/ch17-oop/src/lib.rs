eed9f6031ce997ce2d104ff12c606cbd344677f2
sunday00 <kind65505256@gmail.com>
2024-12-27 01:52:28 +0900

    oop

---
+++ rust/tuto-official/ch17-oop/src/lib.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+pub mod a_base;
+
+pub fn run() {
+    let args: Vec<String> = env::args().collect();
+    let part = args.get(1).unwrap();
+
+    match &part[..] {
+        "base" => {
+            if args.get(2).unwrap().eq("1") {
+                a_base::ch1::exec()
+            }
+        }
+        _ => {}
+    }
+}
---
+++ rust/tuto-official/ch17-oop/src/main.rs
@@ -0,0 +1,5 @@
+use ch17_oop::run;
+
+fn main() {
+    run()
+}
