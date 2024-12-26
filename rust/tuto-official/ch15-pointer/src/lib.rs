7821148797d0a7e1db0abe3105c583baa6910891
sunday00 <kind65505256@gmail.com>
2024-12-26 15:33:27 +0900

    weak pointer

--- rust/tuto-official/ch15-pointer/src/lib.rs
+++ rust/tuto-official/ch15-pointer/src/lib.rs
@@ -5,6 +5,7 @@ mod b_deref;
 mod c_drop;
 mod d_rc;
 mod e_refcell;
+mod f_circular;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
@@ -38,6 +39,13 @@ pub fn run() {
                 e_refcell::ch1::exec();
             } else if args.get(2).unwrap().eq("2") {
                 e_refcell::ch2::exec();
+            }
+        }
+        "circular" => {
+            if args.get(2).unwrap().eq("1") {
+                f_circular::ch1::exec();
+            } else if args.get(2).unwrap().eq("2") {
+                f_circular::ch2::exec();
             }
         }
         _ => {}
