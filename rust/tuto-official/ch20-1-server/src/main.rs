0c9ee1631af036ddc584b2c7eed0f1e416034dfc
sunday00 <kind65505256@gmail.com>
2024-12-31 01:24:43 +0900

    single thread web

--- rust/tuto-official/ch20-1-server/src/main.rs
+++ rust/tuto-official/ch20-1-server/src/main.rs
@@ -14,23 +14,54 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
 
-    // println!("req: {:#?}", http_request);
+    // let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = match buf_reader.lines().next() {
+        Some(r) => r.unwrap(),
+        None => String::from("GET / HTTP/1.1")
+    };
 
-    // let response = "HTTP/1.1 200 OK\r\n\r\n";
+    // if request_line == "GET / HTTP/1.1" {
+    //     let status_line = "HTTP/1.1 200 OK";
+    //     let contents = fs::read_to_string("hello.html").unwrap();
+    //     let length = contents.len();
+    //
+    //     let response =
+    //         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    //
+    //     stream.write_all(response.as_bytes()).unwrap();
+    // } else {
+    //     let status_line = "HTTP/1.1 404 NOT FOUND";
+    //     let contents = fs::read_to_string("404.html").unwrap();
+    //     let length = contents.len();
+    //
+    //     let response = format!(
+    //         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    //     );
+    //
+    //     stream.write_all(response.as_bytes()).unwrap();
+    // }
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-    stream.write_all(response.as_bytes()).unwrap();
+
+    // println!("req: {:#?}", http_request);
+
+    // let response = "HTTP/1.1 200 OK\r\n\r\n";
 }
