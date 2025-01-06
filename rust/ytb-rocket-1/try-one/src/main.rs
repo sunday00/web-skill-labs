37b31345b28e17bb820e8600d7c15098a5436fc4
sunday00 <kind65505256@gmail.com>
2025-01-06 16:37:13 +0900

    asyc await common state

--- rust/ytb-rocket-1/try-one/src/main.rs
+++ rust/ytb-rocket-1/try-one/src/main.rs
@@ -2,40 +2,45 @@
 extern crate rocket;
 
 use reqwest::Client;
+use rocket::futures::TryFutureExt;
 use rocket::http::uri::Origin;
 use rocket::http::Status;
 use rocket::response::Redirect;
 use rocket::serde::json::{json, Value};
 use rocket::serde::{json::Json, Serialize};
+use rocket::State;
 
 const API: Origin<'static> = uri!("/api");
+const REPO: &str = "sunday00/lecture-master";
 
 #[get("/")]
 fn index() -> &'static str {
     "hello rocket"
 }
 
-
-const REPO: &str = "sunday00/web-skill-labs";
 async fn get_latest_release(client: &Client, repo: &str) -> Result<Value, reqwest::Error> {
-    let url = format!("https://api.github.com/repos/{}/release/latest", REPO);
+    let url = format!("https://api.github.com/repos/{}/releases/latest", REPO);
     let res = client.get(&url).send().await?;
 
     // let release: Value = res.json().await?;
-    let release: Value = res.json::<Value>().await?;
+    let release = res.json::<Value>().await?;
+
+    println!("{:#?}", release);
+
+    Ok(release)
 }
 
 #[get("/google-keep-desktop/<_platform>/<current_version>?<msg>")]
-fn google_keep_desktop(_platform: &str, current_version: &str, msg: Option<&str>) -> Result<Value, Status> {
+async fn google_keep_desktop(_platform: &str, current_version: &str, msg: Option<&str>, client: &State<Client>) -> Result<Value, Status> {
     if let Some(_msg) = msg {
-        // TODO
         // return Status::Ok;
         return Err(Status::BadRequest);
     }
 
+    get_latest_release(client, REPO).await.or(Err(Status::NoContent))
 
     // Status::Ok
-    Ok(json!({"msg": current_version}))
+    // Ok(json!({"msg": current_version}))
 }
 
 #[get("/go")]
@@ -52,5 +57,11 @@ fn json_example() -> Value {
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index, redirect_to_home])
+        .manage(
+            Client::builder()
+                .user_agent("reqwest")
+                .build()
+                .unwrap()
+        )
         .mount(API, routes![google_keep_desktop, json_example])
 }
