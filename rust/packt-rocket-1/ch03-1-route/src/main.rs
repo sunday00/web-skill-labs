9fb70a37b1c68029aa6c789fec67a23004a7c6ff
sunday00 <kind65505256@gmail.com>
2025-01-09 12:37:48 +0900

    segment

--- rust/packt-rocket-1/ch03-1-route/src/main.rs
+++ rust/packt-rocket-1/ch03-1-route/src/main.rs
@@ -1,11 +1,11 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::{Build, Rocket, serde::{Serialize, Deserialize, json::Json}};
+use rocket::{serde::{json::Json, Deserialize, Serialize}, Build, Rocket};
 use rocket_okapi::okapi::{schemars, schemars::JsonSchema};
 use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::*};
 
-#[derive(FromForm, Serialize, Deserialize, JsonSchema)]
+#[derive(FromForm, Serialize, Deserialize, JsonSchema, Debug)]
 struct Filters {
     age: u8,
     active: bool,
@@ -22,15 +22,48 @@ fn users(grade: u8, filters: Filters) { /* ... */ }
 #[post("/users/<grade>", data = "<filters>")]
 fn create_users(grade: u8, filters: Json<Filters>) { /* ... */ }
 
+// ======
+#[openapi(tag = "Rank")]
+#[get("/<rank>", rank = 1)]
+fn first(rank: u8) -> String {
+    let result = rank + 10;
+    format!("Your rank is, {}!", result)
+}
+
+#[openapi(tag = "Rank")]
+#[get("/<name>", rank = 2)]
+fn second(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+// ======
+
+#[get("/get", format = "text/plain")]
+fn get() -> &'static str {
+    "GET Request"
+}
+
+#[post("/post", format = "form", data = "<data>")]
+fn post(data: Json<Filters>) -> &'static str {
+    println!("{:?}", data);
+
+    "POST Request"
+}
+
+// ======
+
+
+// ======
 #[launch]
 fn rocket() -> Rocket<Build> {
     // rocket::build().mount("/", routes![user, users])
 
-    rocket::build().mount("/", openapi_get_routes![users, create_users])
+    rocket::build().mount("/", openapi_get_routes![users, create_users, first, second])
         .mount("/docs/", make_swagger_ui(&SwaggerUIConfig {
             url: "../openapi.json".to_owned(),
             ..Default::default()
-        }),)
+        }), )
+        .mount("/no-s/", routes![get, post])
 }
 
 
