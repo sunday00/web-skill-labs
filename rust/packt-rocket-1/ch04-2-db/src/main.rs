d392f93f1595ddb981a0bf9dd4e21a1dcd2c65e0
sunday00 <kind65505256@gmail.com>
2025-01-14 13:00:22 +0900

    sqlite

--- rust/packt-rocket-1/ch04-2-db/src/main.rs
+++ rust/packt-rocket-1/ch04-2-db/src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::http::ContentType;
+use rocket::http::{ContentType, Status};
 use rocket::response::Responder;
 use rocket::serde::json::Json;
 use rocket::serde::{Deserialize, Serialize};
@@ -15,8 +15,8 @@ use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
 use rocket_okapi::{openapi, openapi_get_routes};
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::vec::Vec;
-use sqlx::FromRow;
-use uuid::Uuid;
+use sqlx::{FromRow, SqlitePool};
+use sqlx::sqlite::SqlitePoolOptions;
 
 #[derive(Deserialize)]
 struct Config {
@@ -31,54 +31,54 @@ struct Filters {
 
 #[derive(Serialize, Deserialize, FromForm, JsonSchema, Debug)]
 #[schemars(example = "example_uuid")]
-struct UuidReq(String);
+struct Uuid(String);
 
 fn example_uuid() -> String {
-    String::from("3e3dd4ae-3c37-40c6-aa64-7061f284ce28")
+    String::from("2a1d0e01-a0cd-4e47-b785-8a5c8ed5bafd")
 }
 
-impl<'r> FromParam<'r> for UuidReq {
+impl<'r> FromParam<'r> for Uuid {
     type Error = ();
 
     fn from_param(param: &'r str) -> Result<Self, Self::Error> {
-        Ok(UuidReq(String::from(param)))
+        Ok(Uuid(String::from(param)))
     }
 }
 
 #[derive(Serialize, Deserialize, JsonSchema, Debug, FromRow)]
 #[sqlx(rename_all = "camelCase")]
 struct User {
-    uuid: Uuid,
+    uuid: String,
     name: String,
-    age: u8,
-    grade: u8,
-    // active: bool,
-    #[sqlx(rename="active")]
-    present: bool,
-    #[sqlx(default)]
-    not_in_database: String
+    age: i64,
+    grade: i64,
+    active: bool,
+    // #[sqlx(rename="active")]
+    // present: bool,
+    // #[sqlx(default)]
+    // not_in_database: String
 }
 
-impl<'r> Responder<'r, 'r> for &'r User {
+impl<'r> Responder<'r, 'r> for User {
     fn respond_to(self, req: &'r Request<'_>) -> response::Result<'r> {
         Response::
-        build_from(Json(self).respond_to(req)?)
-            .raw_header("X-USER-ID", self.uuid.to_string())
+        build_from(Json(&self).respond_to(req)?)
+            .raw_header("X-USER-ID", self.uuid)
             .header(ContentType::JSON)
             .ok()
     }
 }
 
-impl<'r> OpenApiResponderInner for &'r User {
+impl<'r> OpenApiResponderInner for User {
     fn responses(gen: &mut OpenApiGenerator) -> rocket_okapi::Result<Responses> {
         <Json<&User>>::responses(gen)
     }
 }
 
 #[derive(Serialize, JsonSchema, Debug)]
-struct Users<'a>(Vec<&'a User>);
+struct Users(Vec<User>);
 
-impl<'r> Responder<'r, 'r> for Users<'r> {
+impl<'r> Responder<'r, 'r> for Users {
     fn respond_to(self, req: &'r Request<'_>) -> response::Result<'r> {
         Response::build_from(Json(self).respond_to(req)?)
             .header(ContentType::JSON)
@@ -86,7 +86,7 @@ impl<'r> Responder<'r, 'r> for Users<'r> {
     }
 }
 
-impl<'r> OpenApiResponderInner for Users<'r> {
+impl<'r> OpenApiResponderInner for Users {
     fn responses(gen: &mut OpenApiGenerator) -> rocket_okapi::Result<Responses> {
         <Json<&Users>>::responses(gen)
     }
@@ -105,32 +105,76 @@ impl VisitorCounter {
 
 #[openapi(tag = "Users")]
 #[get("/user/<uuid>", rank = 1)]
-fn user(counter: &State<VisitorCounter>, uuid: UuidReq) -> Option<&User> {
+async fn user(counter: &State<VisitorCounter>, pool: &rocket::State<SqlitePool>, uuid: Uuid) -> Result<User, Status> {
     // counter.visitor.fetch_add(1, Ordering::Relaxed);
     counter.increment_counter();
     println!("visitors: {}", counter.visitor.load(Ordering::Relaxed));
 
-    USERS.get(uuid.0.as_str())
+    let uuid_param = uuid.0.to_string();
+
+    // USERS.get(uuid.0.to_string().as_str())
+    let user = sqlx::query_as!(User, r#"SELECT * FROM users WHERE uuid = $1"#, uuid_param).fetch_one(pool.inner()).await;
+
+    user.map_err(|_| Status::NotFound)
 }
 
 #[openapi(tag = "Users")]
 #[get("/user", rank = 1)]
-fn users(counter: &State<VisitorCounter>) -> Users {
+async fn users(counter: &State<VisitorCounter>, pool: &rocket::State<SqlitePool>) -> Result<Users, Status> {
     // counter.visitor.fetch_add(1, Ordering::Relaxed);
     counter.increment_counter();
     println!("visitors: {}", counter.visitor.load(Ordering::Relaxed));
 
-    Users(USERS.values().collect())
+    // Users(USERS.values().collect())
+
+    // let mut query_str = String::from("SELECT * FROM users WHERE name LIKE $1 AND grade = $2");
+    let mut query_str = String::from("SELECT * FROM users");
+
+    // if filters.is_some() {
+    //     query_str.push_str(" AND age = $3 AND active = $4");
+    // }
+
+    let mut query = sqlx::query_as::<_, User>(&query_str)
+        // .bind(format!("%{}%", &name_grade.name))
+        // .bind(name_grade.grade as i16)
+    ;
+
+    // if let Some(fts) = &filters {
+    //     query = query.bind(fts.age as i16).bind(fts.
+    //         active);
+    // }
+
+    let unwrapped_users = query.fetch_all(pool.inner()).await;
+    let users: Vec<User> = unwrapped_users.map_err(|_| Status::InternalServerError)?;
+
+    if users.is_empty() {
+        Err(Status::NotFound)
+    } else {
+        Ok(Users(users))
+    }
 }
 
 #[launch]
-fn rocket() -> Rocket<Build> {
+async fn rocket() -> Rocket<Build> {
     let visitor_counter = VisitorCounter {
         visitor: AtomicU64::new(0),
     };
 
-    rocket::build()
-        .manage(visitor_counter)
+    let rocket_frame = rocket::build();
+
+    let config: Config = rocket_frame
+        .figment()
+        .extract()
+        .expect("Incorrect Rocket.toml configuration");
+
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(&config.database_url)
+        .await
+        .expect("Unable to connect to database");
+
+    rocket_frame.manage(visitor_counter)
+        .manage(pool)
         .mount("/", openapi_get_routes![user, users])
         .mount("/docs/", make_swagger_ui(&SwaggerUIConfig {
             url: "../openapi.json".to_owned(),
