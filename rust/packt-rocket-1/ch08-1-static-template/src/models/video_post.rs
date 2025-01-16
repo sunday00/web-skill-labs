1a80541ee681cd8671b12e2cc03e7e8a440e2bc8
sunday00 <kind65505256@gmail.com>
2025-01-16 19:34:22 +0900

    template

---
+++ rust/packt-rocket-1/ch08-1-static-template/src/models/video_post.rs
@@ -0,0 +1,9 @@
+use super::post::Post;
+use crate::traits::DisplayPostContent;
+
+pub struct VideoPost(Post);
+impl DisplayPostContent for VideoPost {
+    fn raw_html() -> String {
+        todo!("will implement later")
+    }
+}
