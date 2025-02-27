cf9ae739ca816c23728f97ff2166d43a45b2af28
sunday00 <kind65505256@gmail.com>
2025-02-27 11:00:33 +0900

    remove tailwind

---
+++ nodejs/remix-series/cra-chakra/app/routes/_index.tsx
@@ -0,0 +1,19 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <h1>HI</h1>
+      </div>
+    </div>
+  );
+}
+
