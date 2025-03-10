93a676535d40521e1a8d39bc287515040c662798
sunday00 <kind65505256@gmail.com>
2025-03-11 00:11:19 +0900

    select complicated

--- nodejs/remix-series/ytb-official-single/app/components/form/select.tsx
+++ nodejs/remix-series/ytb-official-single/app/components/form/select.tsx
@@ -1,7 +1,107 @@
-import { HTMLAttributes, KeyboardEvent, MouseEvent, ReactNode, useRef, useState } from 'react'
+import {
+  HTMLAttributes,
+  KeyboardEvent,
+  MouseEvent,
+  ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import { FaCaretDown } from 'react-icons/fa'
 
 export type SelectOption = { show?: string; value: string | number }
+
+const handleMouse = (
+  el: HTMLDetailsElement | undefined | null,
+  name: string,
+  setCurrentFocus: (focus: number) => void,
+) => {
+  if (!el) return
+
+  setCurrentFocus(0)
+
+  if (el.open) {
+    el.open = false
+    return
+  }
+
+  el.open = true
+  ;(document.querySelector(`.${name}-option-0`) as HTMLInputElement).focus()
+}
+
+const handleArrowDown = (
+  el: HTMLDetailsElement,
+  name: string,
+  currentFocus: number,
+  setCurrentFocus: (focus: number) => void,
+  options: SelectOption[],
+) => {
+  if (!el.open) {
+    el.open = true
+    ;(document.querySelector(`.${name}-option-0`) as HTMLInputElement).focus()
+    return
+  }
+
+  if (el.open) {
+    const newCurrentIndex = currentFocus >= options.length - 1 ? 0 : currentFocus + 1
+    ;(document.querySelector(`.${name}-option-${newCurrentIndex}`) as HTMLInputElement).focus()
+    setCurrentFocus(newCurrentIndex)
+  }
+}
+
+const handleArrowUp = (
+  el: HTMLDetailsElement,
+  name: string,
+  currentFocus: number,
+  setCurrentFocus: (focus: number) => void,
+  options: SelectOption[],
+) => {
+  if (!el.open) {
+    return
+  }
+
+  const newCurrentIndex = currentFocus <= 0 ? options.length - 1 : currentFocus - 1
+  ;(document.querySelector(`.${name}-option-${newCurrentIndex}`) as HTMLInputElement).focus()
+  setCurrentFocus(newCurrentIndex)
+}
+
+const handleEscape = (
+  el: HTMLDetailsElement,
+  name: string,
+  setCurrentFocus: (focus: number) => void,
+) => {
+  setCurrentFocus(0)
+  el.open = false
+  ;(document.querySelector(`.${name}-select`) as HTMLButtonElement).focus()
+}
+
+const handleTab = (
+  el: HTMLDetailsElement,
+  name: string,
+  setCurrentFocus: (focus: number) => void,
+) => {
+  handleEscape(el, name, setCurrentFocus)
+}
+
+const handleBlur = (
+  e: unknown,
+  name: string,
+  el: HTMLDetailsElement,
+  setSelected: (select: SelectOption) => void,
+) => {
+  ;(e as MouseEvent<HTMLElement>).preventDefault()
+
+  const target = (e as MouseEvent<HTMLInputElement>).target as HTMLInputElement
+
+  if (target.className.includes(`${name}-select`)) return
+
+  if (target.className.includes(`${name}-option`)) {
+    setSelected({ show: target.value, value: target.dataset.value! })
+  }
+
+  ;(document.querySelector(`.${name}-select`) as HTMLButtonElement).focus()
+  el.open = false
+}
 
 const Option = ({ name, option, idx }: { name: string; option: SelectOption; idx: number }) => {
   option.show = option.show ?? option.value.toString()
@@ -10,7 +110,7 @@ const Option = ({ name, option, idx }: { name: string; option: SelectOption; idx
     <input
       type={'button'}
       value={option.show}
-      className={`input input-bordered w-full ${name}-option ${name}-option-${idx} text-start focus:outline-none focus:bg-darker hover:bg-darker cursor-pointer`}
+      className={`input w-full ${name}-option ${name}-option-${idx} text-start focus:outline-none focus:bg-darker hover:bg-darker cursor-pointer`}
       readOnly={true}
       data-value={option.value}
       tabIndex={0}
@@ -30,9 +130,9 @@ export default function Select({
   optionClassName?: string
   options: SelectOption[]
   defaultValue?: SelectOption
-  w?: 'fit' | 'full' | string
+  w?: string
 }) {
-  if (defaultValue) defaultValue.show = defaultValue!.show ?? defaultValue!.value.toString()
+  if (defaultValue) defaultValue.show = defaultValue.show ?? defaultValue.value.toString()
   else options[0].show = options[0].show ?? options[0].value.toString()
 
   const wrapRef = useRef<HTMLDetailsElement>(null)
@@ -43,64 +143,36 @@ export default function Select({
   const handleActive = (e: MouseEvent<HTMLElement>) => {
     e.preventDefault()
 
-    if (!wrapRef || !wrapRef.current) return
-
-    setCurrentFocus(0)
-
-    if (wrapRef.current.open) {
-      wrapRef.current.open = false
-      return
-    }
-
-    wrapRef.current.open = true
-    ;(document.querySelector(`.${name}-option-0`) as HTMLInputElement).focus()
+    handleMouse(wrapRef?.current, name, setCurrentFocus)
   }
 
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (!wrapRef || !wrapRef.current) return
+    if (!wrapRef?.current) return
 
-    if (e.key === 'ArrowDown') {
-      if (!wrapRef.current.open) {
-        wrapRef.current.open = true
-        ;(document.querySelector(`.${name}-option-0`) as HTMLInputElement).focus()
+    switch (e.key) {
+      case 'ArrowDown':
+        return handleArrowDown(wrapRef.current, name, currentFocus, setCurrentFocus, options)
+      case 'ArrowUp':
+        return handleArrowUp(wrapRef.current, name, currentFocus, setCurrentFocus, options)
+      case 'Escape':
+        return handleEscape(wrapRef.current, name, setCurrentFocus)
+      case 'Tab':
+        return handleTab(wrapRef.current, name, setCurrentFocus)
+      default:
         return
-      }
-
-      if (wrapRef.current.open) {
-        const newCurrentIndex = currentFocus >= options.length - 1 ? 0 : currentFocus + 1
-        ;(document.querySelector(`.${name}-option-${newCurrentIndex}`) as HTMLInputElement).focus()
-        setCurrentFocus(newCurrentIndex)
-      }
-    }
-
-    if (e.key === 'ArrowUp') {
-      if (!open) {
-        return
-      }
-
-      const newCurrentIndex = currentFocus <= 0 ? options.length - 1 : currentFocus - 1
-      ;(document.querySelector(`.${name}-option-${newCurrentIndex}`) as HTMLInputElement).focus()
-      setCurrentFocus(newCurrentIndex)
-    }
-
-    if (e.key === 'Escape') {
-      setCurrentFocus(0)
-      wrapRef.current.open = false
-      ;(document.querySelector(`.${name}-select`) as HTMLButtonElement).focus()
-    }
-
-    if (e.key === 'Tab') {
-      setCurrentFocus(0)
-      wrapRef.current.open = false
-      ;(document.querySelector(`.${name}-select`) as HTMLButtonElement).focus()
     }
   }
+
+  useEffect(() => {
+    if (!wrapRef.current) return
+    document.querySelector('html')?.addEventListener('click', (e: unknown) => {
+      handleBlur(e, name, wrapRef.current as HTMLDetailsElement, setSelected)
+    })
+  }, [name])
 
   const optionLists = options.map((option: SelectOption, idx) => {
     return <Option name={name} key={option.value} option={option} idx={idx} />
   })
-
-  // TODO: how to blur ?
 
   return (
     <details className="dropdown" ref={wrapRef}>
@@ -117,7 +189,7 @@ export default function Select({
         <FaCaretDown />
       </summary>
       <div
-        className="menu dropdown-content bg-base-100 shadowbg-base-100 rounded-box z-[1] w-full p-2 shadow"
+        className="menu dropdown-content bg-base-100 shadowbg-base-100 rounded-b-lg z-[1] w-full p-2 shadow"
         role="button"
         tabIndex={0}
         onKeyDown={handleKeyDown}
