291385ddb7b3547ec3da3fae267e6796c452d0ef
sunday00 <kind65505256@gmail.com>
2025-04-07 15:12:22 +0900

    stream

---
+++ nodejs/stream-ytb-prac01/src/entities/ExampleVideo.js
@@ -0,0 +1,5 @@
+export default {
+  path: `./public/SHORT.mp4`,
+  duration: 13,
+  size: 2_107_842,
+}
---
+++ nodejs/stream-ytb-prac01/src/server.js
@@ -0,0 +1,4 @@
+import ExpressService from './services/express-service.js'
+
+const ex = new ExpressService()
+ex.start()
