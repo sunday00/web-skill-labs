90005c6ae1895806f58bc14a1d2adb9a9c344941
sunday00 <kind65505256@gmail.com>
2025-08-25 18:59:39 +0900

    hm

--- nodejs/fcm-receiver/public/firebase-messaging-sw.js
+++ nodejs/fcm-receiver/public/firebase-messaging-sw.js
@@ -14,35 +14,35 @@ self.addEventListener("activate", function (e) {
   console.log("fcm service worker가 실행되었습니다.");
 });
 
-// self.addEventListener("push", (event) => {
-//   console.log(event)
-//   const payload = JSON.parse(event.data.text());
-//   event.waitUntil(
-//       registration.showNotification(payload.title, {
-//         body: payload.body,
-//         data: { link: payload.link },
-//       })
-//   );
-// });
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAYLae0yPW5PLeIlcCpGu_tVB5PrDoDs3I",
-//   authDomain: "iron-common.firebaseapp.com",
-//   projectId: "iron-common",
-//   storageBucket: "iron-common.firebasestorage.app",
-//   messagingSenderId: "51367295915",
-//   appId: "1:51367295915:web:94446d8310c6bdafb5b0ee"
-// };
+self.addEventListener("push", (event) => {
+  console.log(event)
+  const payload = JSON.parse(event.data.text());
+  event.waitUntil(
+      registration.showNotification(payload.title, {
+        body: payload.body,
+        data: { link: payload.link },
+      })
+  );
+});
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCM7zTa6BrXUA6S4EjhMg68hSYAOIIYns4",
-  authDomain: "everybody-proam-9d4ac.firebaseapp.com",
-  projectId: "everybody-proam-9d4ac",
-  storageBucket: "everybody-proam-9d4ac.firebasestorage.app",
-  messagingSenderId: "262426257724",
-  appId: "1:262426257724:web:054d8eab20c7507b181639",
-  measurementId: "G-J3XDCH0W1H"
+  apiKey: "AIzaSyAYLae0yPW5PLeIlcCpGu_tVB5PrDoDs3I",
+  authDomain: "iron-common.firebaseapp.com",
+  projectId: "iron-common",
+  storageBucket: "iron-common.firebasestorage.app",
+  messagingSenderId: "51367295915",
+  appId: "1:51367295915:web:94446d8310c6bdafb5b0ee"
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCM7zTa6BrXUA6S4EjhMg68hSYAOIIYns4",
+//   authDomain: "everybody-proam-9d4ac.firebaseapp.com",
+//   projectId: "everybody-proam-9d4ac",
+//   storageBucket: "everybody-proam-9d4ac.firebasestorage.app",
+//   messagingSenderId: "262426257724",
+//   appId: "1:262426257724:web:054d8eab20c7507b181639",
+//   measurementId: "G-J3XDCH0W1H"
+// };
 
 // const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
--- nodejs/fcm-receiver/src/App.tsx
+++ nodejs/fcm-receiver/src/App.tsx
@@ -19,24 +19,24 @@ async function handleAllowNotification() {
     });
   }
 
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyAYLae0yPW5PLeIlcCpGu_tVB5PrDoDs3I",
-  //   authDomain: "iron-common.firebaseapp.com",
-  //   projectId: "iron-common",
-  //   storageBucket: "iron-common.firebasestorage.app",
-  //   messagingSenderId: "51367295915",
-  //   appId: "1:51367295915:web:94446d8310c6bdafb5b0ee"
-  // };
-
   const firebaseConfig = {
-    apiKey: "AIzaSyCM7zTa6BrXUA6S4EjhMg68hSYAOIIYns4",
-    authDomain: "everybody-proam-9d4ac.firebaseapp.com",
-    projectId: "everybody-proam-9d4ac",
-    storageBucket: "everybody-proam-9d4ac.firebasestorage.app",
-    messagingSenderId: "262426257724",
-    appId: "1:262426257724:web:054d8eab20c7507b181639",
-    measurementId: "G-J3XDCH0W1H"
+    apiKey: "AIzaSyAYLae0yPW5PLeIlcCpGu_tVB5PrDoDs3I",
+    authDomain: "iron-common.firebaseapp.com",
+    projectId: "iron-common",
+    storageBucket: "iron-common.firebasestorage.app",
+    messagingSenderId: "51367295915",
+    appId: "1:51367295915:web:94446d8310c6bdafb5b0ee"
   };
+
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyCM7zTa6BrXUA6S4EjhMg68hSYAOIIYns4",
+  //   authDomain: "everybody-proam-9d4ac.firebaseapp.com",
+  //   projectId: "everybody-proam-9d4ac",
+  //   storageBucket: "everybody-proam-9d4ac.firebasestorage.app",
+  //   messagingSenderId: "262426257724",
+  //   appId: "1:262426257724:web:054d8eab20c7507b181639",
+  //   measurementId: "G-J3XDCH0W1H"
+  // };
 
   const app = initializeApp(firebaseConfig);
   const messaging = getMessaging(app);
@@ -47,8 +47,8 @@ async function handleAllowNotification() {
 
   const token = await getToken(messaging, {
     vapidKey:
-      // "BM_cB08_xpXfzGrkUcJr79AncK4oQ8T6Jl9y6oCuMD6rIInsZiIGYeVB7jyI5blidjF32VkQLTg4_MapgZxQwlE",
-      "BItpeVl0shaaXrFUDa5WwyrQEAQg5tQQlPdKoP8ObPiwGp4HFuTl4HrSRfFpldql2YFP0q86SgcJjLXyvFgMD00",
+      "BM_cB08_xpXfzGrkUcJr79AncK4oQ8T6Jl9y6oCuMD6rIInsZiIGYeVB7jyI5blidjF32VkQLTg4_MapgZxQwlE",
+      // "BItpeVl0shaaXrFUDa5WwyrQEAQg5tQQlPdKoP8ObPiwGp4HFuTl4HrSRfFpldql2YFP0q86SgcJjLXyvFgMD00",
   });
 
   console.log(token);
