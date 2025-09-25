9eb795d300ff3b31d21f3f43b07446669040d43a
sunday00 <kind65505256@gmail.com>
2025-09-25 17:20:41 +0900

    lua rain

---
+++ gleam/Exercism/lua/raindrops/raindrops.lua
@@ -0,0 +1,22 @@
+return function(n)
+    local r = ""
+
+    if n % 3 == 0 then
+        r = r .. "Pling"
+    end
+
+    if n % 5 == 0 then
+        r = r .. "Plang"
+    end
+
+    if n % 7 == 0 then
+        r = r .. "Plong"
+    end
+
+    if #r == 0 then
+        return tostring(n)
+    else
+        return r
+    end
+
+end
