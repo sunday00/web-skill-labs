689c7b4b5a06816e607176349b13d7a993da556e
sunday00 <kind65505256@gmail.com>
2025-10-13 19:14:54 +0900

    chiper

--- gleam/Exercism/lua/atbash-cipher/atbash-cipher.lua
+++ gleam/Exercism/lua/atbash-cipher/atbash-cipher.lua
@@ -1,4 +1,46 @@
-return {
-  encode = function(plaintext)
-  end
+local o = {
+    encode = function(plaintext)
+        local abc = 'abcdefghijklmnopqrstuvwxyz'
+        local cba = string.gmatch(string.reverse(abc), '.')
+
+        local toTable = function(i)
+            local t = {}
+
+            for n in i do
+                table.insert(t, n)
+            end
+
+            return t
+        end
+
+        cba = toTable(cba)
+
+        local res = ''
+        for ch in string.gmatch(plaintext, '.') do
+            if ch == ' ' or ch == ',' or ch == '.' then
+                res = res
+            else
+
+                local chi = string.find(abc, string.lower(ch))
+
+                if chi ~= nil then
+                    if (#res + 1) % 6 == 0 then
+                        res = res .. ' ' .. string.lower(cba[chi])
+                    else
+                        res = res .. string.lower(cba[chi])
+                    end
+                else
+                    res = res .. ch
+                end
+            end
+
+
+        end
+
+        return res
+    end
 }
+
+--print(o.encode('The quick brown fox jumps over the lazy dog.'))
+
+return o
