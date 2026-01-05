7a2f962a992fddb69a9948af361ea380b3d6b12e
sunday00 <kind65505256@gmail.com>
2026-01-05 14:56:55 +0900

    kakaka

--- gleam/Exercism/lua/transpose/transpose.lua
+++ gleam/Exercism/lua/transpose/transpose.lua
@@ -44,52 +44,45 @@ function transpose (s)
     local horizontals = string.split(string.replace(s, '\n', '|\n'), '\n')
 
     local maxLen = 0
-    local maxLenIdx = 0
-    for i, ho in ipairs(horizontals) do
+    for _, ho in ipairs(horizontals) do
         if maxLen <= #string.replace(ho, '|', '') then
             maxLen = #string.replace(ho, '|', '')
-            maxLenIdx = i
         end
     end
 
     for i = 1, maxLen do
         local v = ''
+        local lastVI = 1
+        for y = 1, #horizontals do
+            if i <= #horizontals[y] then
+                lastVI = y
+            end
+        end
+
         for y, ho in ipairs(horizontals) do
             local letter = string.sub(ho, i, i)
 
-            if y > maxLenIdx and i > maxLenIdx and (letter == '' or letter == '|') then
-                goto continue
+            if (letter == '' or letter == '|') and y <= lastVI then
+                v = v .. ' '
+
+            else
+
+                v = v .. letter
             end
-
-            if (letter == '' or letter == '|') then
-                letter = ' '
-            end
-
-            v = v .. letter
-
-            :: continue ::
         end
         table.insert(verticals, v)
-        print('"' .. v .. '"')
+        --print('"' .. v .. '"')
     end
 
     return string.join(verticals, '\n')
 end
 
-local x = transpose("Chor. Two households, both alike in dignity,\n" ..
-        "In fair Verona, where we lay our scene,\n" ..
-        "From ancient grudge break to new mutiny,\n" ..
-        "Where civil blood makes civil hands unclean.\n" ..
-        "From forth the fatal loins of these two foes\n" ..
-        "A pair of star-cross'd lovers take their life;\n" ..
-        "Whose misadventur'd piteous overthrows\n" ..
-        "Doth with their death bury their parents' strife.\n" ..
-        "The fearful passage of their death-mark'd love,\n" ..
-        "And the continuance of their parents' rage,\n" ..
-        "Which, but their children's end, naught could remove,\n" ..
-        "Is now the two hours' traffic of our stage;\n" ..
-        "The which if you with patient ears attend,\n" ..
-        "What here shall miss, our toil shall strive to mend.")
+--local x = transpose('T\n' ..
+--        'EE\n' ..
+--        'AAA\n' ..
+--        'SSSS\n' ..
+--        'EEEEE\n' ..
+--        'RRRRRR')
 
 --table.debug(x)
 --print(x)
