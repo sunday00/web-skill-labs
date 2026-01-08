579bd7febf0395ed5ea0d9522298c3ca708607e0
sunday00 <kind65505256@gmail.com>
2026-01-08 15:22:52 +0900

    unused value ignore lint

--- nodejs/react-graphql-any/src/views/infra/u-firebase/remote-config/index.tsx
+++ nodejs/react-graphql-any/src/views/infra/u-firebase/remote-config/index.tsx
@@ -1,12 +1,12 @@
 import { Spinner } from '@chakra-ui/react'
 import { useRemoteConfig } from '@/utils/firebase/hook.remote-config.tsx'
 
-const FireBaseRemoteConfig = () => {
+const FireBaseRemoteConfig = (_props: { a?: string }) => {
   const { isReady, getRCValue } = useRemoteConfig()
 
   if (!isReady) return <Spinner />
 
-  const v1 = getRCValue('non_g').asString()
+  const _v1 = getRCValue('non_g').asString()
   const v2 = getRCValue('ccc').asString()
 
   return <>{v2}</>
