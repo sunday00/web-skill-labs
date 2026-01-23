ed45d1fda81a729069538e289919848e314d4e97
sunday00 <kind65505256@gmail.com>
2026-01-23 18:01:34 +0900

    ?

--- gleam/Exercism/lua/palindrome-products/palindrome-products.lua
+++ gleam/Exercism/lua/palindrome-products/palindrome-products.lua
@@ -1,9 +1,40 @@
 local function smallest(min, max)
+    local value = nil
+    local x1 = nil
+    local x2 = nil
 
+    for i = min, max do
+        for ii = i, max do
+            local x = tostring(i * ii)
+
+            for xi = 1, math.floor(#x / 2) do
+                if x[xi] ~= x[#x - (xi) + 1] then
+                    goto continue
+                end
+            end
+
+            value = i * ii
+            x1 = i
+            x2 = ii
+
+            print(value)
+
+            goto breakOut
+
+            :: continue ::
+        end
+    end
+
+    :: breakOut ::
+
+    print(value, x1, x2)
 end
 
 local function largest(min, max)
 
 end
+
+local r = smallest(1000, 9999)
+print(r)
 
 return { smallest = smallest, largest = largest }
