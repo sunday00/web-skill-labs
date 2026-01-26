48f520593380b16d1711cc13aca7446d722799a5
sunday00 <kind65505256@gmail.com>
2026-01-26 11:20:47 +0900

    h,

--- gleam/Exercism/lua/palindrome-products/palindrome-products.lua
+++ gleam/Exercism/lua/palindrome-products/palindrome-products.lua
@@ -1,40 +1,94 @@
+local function getFactorsInRange(x, min, max)
+    local f = {}
+
+    for i = min, max do
+        if i > 1 and i > (x / 2) then
+            break
+        end
+
+        for ii = i, max do
+            if i > 1 and ii > 1 and ii > (x / 2) then
+                break
+            end
+
+            if i * ii == x then
+                table.insert(f, { i, ii })
+            end
+        end
+    end
+
+    return f
+end
+
 local function smallest(min, max)
+    if min > max then
+        error('min must be <= max')
+    end
+
     local value = nil
-    local x1 = nil
-    local x2 = nil
 
     for i = min, max do
         for ii = i, max do
             local x = tostring(i * ii)
 
             for xi = 1, math.floor(#x / 2) do
-                if x[xi] ~= x[#x - (xi) + 1] then
+                if x:sub(xi, xi) ~= x:sub(#x - (xi) + 1, #x - (xi) + 1) then
                     goto continue
                 end
             end
 
-            value = i * ii
-            x1 = i
-            x2 = ii
-
-            print(value)
-
-            goto breakOut
+            if value == nil or value > i * ii then
+                value = i * ii
+            end
 
             :: continue ::
         end
     end
 
-    :: breakOut ::
+    if value == nil then
+        return { value = nil, factors = {} }
+    end
 
-    print(value, x1, x2)
+    local f = getFactorsInRange(value, min, max)
+
+    return { value = value, factors = f }
 end
 
 local function largest(min, max)
+    if min > max then
+        error('min must be <= max')
+    end
 
+    local value = nil
+
+    for i = max, min, -1 do
+        for ii = i, min, -1 do
+            local x = tostring(i * ii)
+
+            for xi = 1, math.floor(#x / 2) do
+                if x:sub(xi, xi) ~= x:sub(#x - (xi) + 1, #x - (xi) + 1) then
+                    goto continue
+                end
+            end
+
+            if value == nil or value < i * ii then
+                value = i * ii
+            end
+
+            :: continue ::
+        end
+    end
+
+    if value == nil then
+        return { value = nil, factors = {} }
+    end
+
+    local f = getFactorsInRange(value, min, max)
+
+    return { value = value, factors = f }
 end
 
-local r = smallest(1000, 9999)
-print(r)
+local r = largest(1000, 9999)
+print(r.value, r.factors[1])
 
 return { smallest = smallest, largest = largest }
