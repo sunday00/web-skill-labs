c308249cf6ff4f7fc0beb07ebcccbfa2f44449d2
sunday00 <kind65505256@gmail.com>
2026-02-10 18:30:31 +0900

    gs

--- Exercism/lua/high-scores/high-scores.lua
+++ Exercism/lua/high-scores/high-scores.lua
@@ -1,26 +1,48 @@
 local HighScores = {}
 
-function HighScores:scores()
+local gScores = {}
 
+function HighScores:scores()
+    return gScores
 end
 
 function HighScores:latest()
-
+    return gScores[#gScores]
 end
 
 function HighScores:personal_best()
+    local max = 0
 
+    for _, v in ipairs(gScores) do
+        max = math.max(max, v)
+    end
+
+    return max
 end
 
 function HighScores:personal_top_three()
+    local inner = {}
 
+    for _, v in ipairs(gScores) do
+        table.insert(inner, v)
+    end
+
+    table.sort(inner)
+
+    local innnner = {}
+    for i = 1, math.min(#inner, 3) do
+        table.insert(innnner, inner[#inner - (i - 1)])
+    end
+
+    return innnner
 end
 
 return function(scores)
-  local high_scores = {}
-  setmetatable(high_scores, { __index = HighScores })
+    local high_scores = {}
 
-  -- TODO: store the scores in the high_scores object
+    gScores = scores
 
-  return high_scores
+    setmetatable(high_scores, { __index = HighScores })
+
+    return high_scores
 end
