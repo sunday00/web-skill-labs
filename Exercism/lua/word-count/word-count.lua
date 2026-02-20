f70102d3bb03db3bed14e977bed7ccfaa6afe240
sunday00 <kind65505256@gmail.com>
2026-02-20 15:10:23 +0900

    wc

--- Exercism/lua/word-count/word-count.lua
+++ Exercism/lua/word-count/word-count.lua
@@ -1,4 +1,21 @@
 local function count_words(s)
+    local ss = (' ' .. s .. ' '):gsub('[^%w\'%s]', ' '):gsub("%s'", ' '):gsub("'%s", ' '):lower()
+    local sss = {}
+
+    for w in string.gmatch(ss, '([^%s]+)') do
+        if sss[w] == nil then
+            sss[w] = 1
+        else
+            sss[w] = sss[w] + 1
+        end
+    end
+
+    return sss
 end
+
+--local r = count_words("'First: don't laugh. Then: don't cry. You're getting it.'")
+--for k, v in pairs(r) do
+--    print(k, v)
+--end
 
 return { count_words = count_words }
