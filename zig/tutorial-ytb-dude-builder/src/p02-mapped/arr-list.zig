a806e17b7b4749619a84de030675c081f4821626
sunday00 <kind65505256@gmail.com>
2026-04-06 11:28:29 +0900

    half time

--- zig/tutorial-ytb-dude-builder/src/p02-mapped/arr-list.zig
+++ zig/tutorial-ytb-dude-builder/src/p02-mapped/arr-list.zig
@@ -3,36 +3,63 @@ const builtin = @import("builtin");
 const u = @import("../custom_utils.zig");
 const lib = @import("../lib.zig");
 
-fn printCharList(T: type, l: std.ArrayList(T)) !void {
-    std.debug.print("check inside: [", .{});
+fn methods(comptime T: type) type {
+    return struct {
+        fn printCharList(l: std.ArrayList(T)) !void {
+            std.debug.print("check inside: [", .{});
 
-    for (l.items) |value| {
-        std.debug.print("{c}", .{value});
-    }
+            for (l.items) |value| {
+                std.debug.print("{c} ", .{value});
+            }
 
-    std.debug.print("] \n\n", .{});
-}
+            std.debug.print("] \n\n", .{});
+        }
 
-fn basic(alloc: std.mem.Allocator, T: type) !std.ArrayList(T) {
-    var l: std.ArrayList(T) = .empty;
+        fn basic(alloc: std.mem.Allocator) !std.ArrayList(T) {
+            var l: std.ArrayList(T) = .empty;
 
-    // try l.append(alloc, "hello");
-    for ("hello, World!") |byte| try l.append(alloc, byte);
+            // try l.append(alloc, "hello");
+            for ("hello, World!") |byte| try l.append(alloc, byte);
 
-    return l;
-}
+            return l;
+        }
 
-fn usingWriter(T: type, alloc: std.mem.Allocator, l: *std.ArrayList(T)) !void {
-    const w = l.writer(alloc);
-    try w.print("?, {s}", .{"Huh?"});
-}
+        fn usingWriter(alloc: std.mem.Allocator, l: *std.ArrayList(T)) !void {
+            const w = l.writer(alloc);
+            try w.print("?, {s}", .{"Huh?"});
+        }
 
-fn usingPopIter(T: type, l: *std.ArrayList(T)) !void {
-    while (l.pop()) |b| {
-        std.debug.print("{c}", .{b});
-    }
+        fn usingPopIter(l: *std.ArrayList(T)) !void {
+            while (l.pop()) |b| {
+                std.debug.print("{c}", .{b});
+            }
 
-    std.debug.print("\n\n", .{});
+            std.debug.print("\n\n", .{});
+        }
+
+        fn addpendSlice(alloc: std.mem.Allocator, l: *std.ArrayList(T)) !void {
+            try l.appendSlice(alloc, "Another greet. Say Hi again");
+        }
+
+        fn removeByIndex(l: *std.ArrayList(T), i: usize) !T {
+            return l.orderedRemove(i);
+        }
+
+        fn swapRemove(l: *std.ArrayList(T), i: usize) !T {
+            // swap remove
+            // remove i
+            // remove tail
+            // set i to tail.
+            //
+            // ex: [a, b, c, d, e]
+            // list.swapRemove(2)
+            // // remain [a. b, d, e] and throw away c
+            // // [a, b, d] and last is e
+            // // [a, b, e, d] is done. e is now located originally c idx.
+
+            return l.swapRemove(i);
+        }
+    };
 }
 
 pub fn main() !void {
@@ -40,22 +67,39 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    var l1 = try basic(alloc, u8);
+    const m = methods(u8);
+
+    var l1 = try m.basic(alloc);
     defer l1.deinit(alloc);
 
-    try printCharList(u8, l1);
+    try m.printCharList(l1);
 
     _ = l1.pop();
 
-    try printCharList(u8, l1);
+    try m.printCharList(l1);
 
-    try usingWriter(u8, alloc, &l1);
+    try m.usingWriter(alloc, &l1);
 
-    try printCharList(u8, l1);
+    try m.printCharList(l1);
 
-    try usingPopIter(u8, &l1);
+    try m.usingPopIter(&l1);
 
-    try printCharList(u8, l1);
+    try m.printCharList(l1);
 
-    // TODO: appendSlice
+    try m.addpendSlice(alloc, &l1);
+
+    try m.printCharList(l1);
+
+    const removed = try m.removeByIndex(&l1, 2);
+    u.print("{c}", .{removed});
+    try m.printCharList(l1);
+
+    const removed2 = try m.swapRemove(&l1, 2);
+    u.print("{c}", .{removed2});
+    try m.printCharList(l1);
+
+    l1.items[2] = 'X'; // <--- directly insert possible.
+    try m.printCharList(l1);
+
+    // TODO: owned slice
 }
