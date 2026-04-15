328933311b959f62dee62c806ec04ee77bac0a50
sunday00 <kind65505256@gmail.com>
2026-04-15 16:56:38 +0900

    json parse

--- zig/practice-raylib/src/tcp/get.zig
+++ zig/practice-raylib/src/tcp/get.zig
@@ -3,7 +3,14 @@ const rl = @import("raylib");
 
 const global = @import("../_common/global.zig");
 
-const Schema = std.json.ArrayHashMap(struct { id: u8 = 0, value: []u8 = undefined });
+const Schema = struct {
+    id: u8,
+    value: []u8,
+
+    pub fn format(self: @This(), writer: *std.Io.Writer) std.Io.Writer.Error!void {
+        try writer.print("id: {}, value: {s}\n", self);
+    }
+};
 
 pub fn run() !void {
     var buff: [4096]u8 = undefined;
@@ -24,7 +31,10 @@ pub fn run() !void {
     const body = try res.reader(&buff).readAlloc(global.g.alloc, @intCast(res.head.content_length orelse 0));
     defer global.g.alloc.free(body);
 
-    std.debug.print("{s}\n", .{body});
+    const example = try std.json.parseFromSlice(Schema, global.g.alloc, body, .{ .ignore_unknown_fields = true });
+    defer example.deinit();
+
+    std.debug.print("{f}\n", .{example.value});
 
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
