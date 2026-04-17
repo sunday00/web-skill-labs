39204131da206b624b3e9e0aad25e852cd1921db
sunday00 <kind65505256@gmail.com>
2026-04-17 16:08:53 +0900

    remove comments

--- zig/practice-raylib/src/handle-resource/font.zig
+++ zig/practice-raylib/src/handle-resource/font.zig
@@ -11,28 +11,13 @@ pub fn run() !void {
     // rl.setTraceLogLevel(rl.TraceLogLevel.all);
 
     const font_data align(8) = @embedFile("../assets/neodgm_code.ttf");
-    // const font_data align(8) = @embedFile("../assets/silkscreen.ttf");
     var codepoints: std.ArrayList(i32) = .empty;
-    // var codepoints = try std.ArrayList(i32).initCapacity(
-    //     alloc,
-    //     0,
-    // );
 
     for (32..127) |i| try codepoints.append(alloc, @intCast(i));
-    // for (32..33) |i| try codepoints.append(global.g.alloc, @intCast(i));
     var cp: i32 = 0xAC00;
     while (cp <= 0xD7A3) : (cp += 1) {
-        // while (cp <= 0xAD00) : (cp += 1) {
-        try codepoints.append(alloc, cp);
+        try codepoints.append(global.g.alloc, cp);
     }
-    // try codepoints.append(global.g.alloc, 0xAD00);
-
-    // try codepoints.ensureTotalCapacity(alloc, 1);
-
-    // codepoints.appendAssumeCapacity('누');
-    // codepoints.appendAssumeCapacity('르');
-    // codepoints.appendAssumeCapacity('시');
-    // codepoints.appendAssumeCapacity('오');
     defer codepoints.deinit(alloc);
 
     const font = try rl.loadFontFromMemory(
@@ -41,14 +26,6 @@ pub fn run() !void {
         11,
         codepoints.items,
     );
-
-    // const font = try rl.loadFontFromMemory(
-    //     ".ttf",
-    //     font_data,
-    //     15,
-    //     null,
-    // );
-    // defer rl.unloadFont(font);
 
     // rl.setTextureFilter(font.texture, .bilinear);
 
