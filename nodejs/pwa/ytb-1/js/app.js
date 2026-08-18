8156fb098d583f738bf236046ec10157f05aca7e
sunday00 <kind65505256@gmail.com>
2026-08-18 16:15:43 +0900

    caches

--- nodejs/pwa/ytb-1/js/app.js
+++ nodejs/pwa/ytb-1/js/app.js
@@ -3,3 +3,20 @@ if ('serviceWorker' in navigator) {
         .then(reg => console.log('serviceWorker registered', reg))
         .catch(e => console.error('failed to register serviceWorker', e))
 }
+
+// function separator(strings, ...variables) {
+//     console.log({strings, variables})
+//
+//     let result = strings.raw[0];
+//     for (let i = 0; i < variables.length; i++) {
+//         result += variables[i] + strings.raw[i + 1];
+//     }
+//
+//     console.log(result);
+// }
+//
+// const prefix = 'OH!'
+// const name = 'kim'
+// const age = 21
+// separator`${prefix} hello, ${name}, you are ${age}, so you can pass.`
+
--- nodejs/pwa/ytb-1/sw.js
+++ nodejs/pwa/ytb-1/sw.js
@@ -1,5 +1,22 @@
 // console.log('service worker inside sw.js')
 
+const cacheName = 'app-shell-rsrs'
+const assets = [
+    'index.html', '/',
+    'js/app.js',
+    'js/common.js',
+    'js/materialize.min.js',
+    'css/styles.css',
+    'css/materialize.min.css',
+    'img/pkcontacts.png',
+    'img/favicon.svg',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+]
+
+caches.open(cacheName).then(cache => {
+    cache.addAll(assets)
+})
+
 self.addEventListener('install', evt => {
     console.log('serviceWorker installed')
 })
