b7c97bab6a96d4c2a2d64b186d6c39cae74c1e9e
sunday00 <kind65505256@gmail.com>
2026-08-18 14:46:29 +0900

    node pwa

--- nodejs/pwa/ytb-1/sw.js
+++ nodejs/pwa/ytb-1/sw.js
@@ -3,3 +3,11 @@
 self.addEventListener('install', evt => {
     console.log('serviceWorker installed')
 })
+
+self.addEventListener('activate', evt => {
+    console.log('serviceWorker activated')
+})
+
+self.addEventListener('fetch', (evt) => {
+    console.log(evt)
+})
