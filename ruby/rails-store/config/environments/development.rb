c54bf48394c1459aa882d37c811d3b3dc8bd86f1
sunday00 <kind65505256@gmail.com>
2026-09-03 14:03:40 +0900

    taste done

--- ruby/rails-store/config/environments/development.rb
+++ ruby/rails-store/config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 4040 }
 
-  #config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #     address:              'smtp.gmail.com',
   #     port:                 587,
